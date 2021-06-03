ac93257b34223bbfa5b700abc5afcc04f6b6b7d6
Victoria Kruczek <victoria.kruczek@gmail.com>
2021-06-03 09:04:12 +0200

    Add quote excercices

---
+++ ES6 Javascript/quotes.js
@@ -0,0 +1,15 @@
+goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
+badStr = 'Finn responds, "Let's go!"';
+
+my Var = 'FirstLine\n\t\\SecondLine\nThirdLine'
+/* Output:
+FirstLine
+ 	\SecondLine
+ThirdLine */
+
+/*
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
+*/
